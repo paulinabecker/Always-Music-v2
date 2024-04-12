4c2737d36f09c9043b77d852207977a1d5e366a7
Paulina Becker <paulina-becker@hotmail.com>
2024-04-12 17:23:27 -0400

    always music v2

---
+++ README.txt
@@ -0,0 +1,20 @@
+Descripción
+La escuela de música Always Music solicitó hacer unas pruebas con el avance del desarrollo
+del sistema de gestión con base de datos PostgreSQL, se dieron cuenta que no se podían
+hacer varias consultas de forma simultánea y que al intentar hacer una consulta errónea, no
+recibían ningún error, dejando la posibilidad de creer que un estudiante fue registrado y que
+esto no sea así.
+En este desafío deberás ocupar la clase Pool definiendo sus diferentes propiedades, capturar
+los posibles errores en el proceso de conexión con la base de datos y realizar las siguientes
+consultas usando textos parametrizados.
+● Agregar un nuevo estudiante.
+● Consultar los estudiantes registrados.
+● Consultar estudiante por rut.
+● Actualizar la información de un estudiante.
+● Eliminar el registro de un estudiante.
+
+Requerimientos
+1. Hacer todas las consultas con un JSON como argumento del método query.
+2. Hacer las consultas con texto parametrizado.
+3. Capturar los posibles errores en todas las consultas e imprimirlos por consola.
+4. Obtener el registro de los estudiantes registrados en formato de arreglos.
---
+++ index.js
@@ -0,0 +1,140 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const config = {
+  user: "postgres",
+  host: "localhost",
+  database: "always music",
+  password: "Pauli1989+",
+  port: 5432,
+};
+
+const pool = new Pool(config);
+
+// Función para manejar errores
+function handleError(error) {
+  console.error('Error:', error.message);
+}
+
+// Función para registrar un nuevo estudiante
+async function registrarEstudiante(nombre, rut, curso, nivel) {
+  try {
+    const query = {
+      text: 'INSERT INTO estudiantes(nombre, rut, curso, nivel) VALUES($1, $2, $3, $4)',
+      values: [nombre, rut, curso, nivel]
+    };
+    const result = await pool.query(query);
+    console.log('Estudiante registrado correctamente.');
+    return result.rows;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// Función para obtener un estudiante por su rut
+async function obtenerEstudiantePorRut(rut) {
+  try {
+    const query = {
+      text: 'SELECT * FROM estudiantes WHERE rut = $1',
+      values: [rut]
+    };
+    const result = await pool.query(query);
+
+    if (result.rows.length === 0) {
+      throw new Error('No se encontró ningún estudiante con el rut proporcionado.');
+    }
+
+    console.log(result.rows);
+    return result.rows;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// Función para obtener todos los estudiantes
+async function obtenerTodosLosEstudiantes() {
+  try {
+    const query = {
+      text: 'SELECT * FROM estudiantes'
+    };
+    const result = await pool.query(query);
+
+    if (result.rows.length === 0) {
+      throw new Error('No se encontraron estudiantes.');
+    }
+
+    console.log(result.rows);
+    return result.rows;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// Función para actualizar los datos de un estudiante
+async function actualizarEstudiante(nombre, rut, curso, nivel) {
+  try {
+    const query = {
+      text: 'UPDATE estudiantes SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4',
+      values: [nombre, curso, nivel, rut]
+    };
+    const result = await pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new Error('No se encontró ningún estudiante para actualizar.');
+    }
+
+    console.log('Estudiante actualizado correctamente.');
+    return result.rows;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// Función para eliminar un estudiante por su rut
+async function eliminarEstudiantePorRut(rut) {
+  try {
+    const query = {
+      text: 'DELETE FROM estudiantes WHERE rut = $1',
+      values: [rut]
+    };
+    const result = await pool.query(query);
+
+    if (result.rowCount === 0) {
+      throw new Error('No se encontró ningún estudiante para eliminar.');
+    }
+
+    console.log('Estudiante eliminado correctamente.');
+    return result.rows;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// Obtener registros de todos los estudiantes registrados en formato de arreglo
+async function obtenerRegistrosEstudiantes() {
+  try {
+    const query = {
+      text: 'SELECT array_agg(row_to_json(estudiantes)) AS registros FROM estudiantes'
+    };
+    const result = await pool.query(query);
+
+    if (!result.rows[0].registros) {
+      throw new Error('No se encontraron registros de estudiantes.');
+    }
+
+    console.log(result.rows[0].registros);
+    return result.rows[0].registros;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+
+/*CONSULTAS
+registrarEstudiante('Camila Salas', '24576891-8', 'Guitarra', 'Principiante');
+obtenerEstudiantePorRut('34567890-1'); 
+obtenerTodosLosEstudiantes();
+actualizarEstudiante('Pedro García', '34567890-1', 'Batería', 'Intermedio');
+eliminarEstudiantePorRut('24576891-8');  
+*/ 
+
